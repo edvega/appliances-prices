4735b0329e7632e043baa592f1de62054049d98a
Edwin Vega <edwega@gmail.com>
2019-02-24 18:38:16 -0600

    Modified XPath value for OrderBy web element

--- appliances/src/test/java/com/github/edvega/appliances/pages/AppliancesPage.java
+++ appliances/src/test/java/com/github/edvega/appliances/pages/AppliancesPage.java
@@ -2,7 +2,6 @@ package com.github.edvega.appliances.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import com.github.edvega.appliances.utilities.BasePage;
@@ -12,7 +11,7 @@ public class AppliancesPage extends BasePage {
 	private By filterHeader = By.xpath("/html/body/div[3]/div/div[2]/div[1]/div/div/div/div/h2");
 	private By productsCounter = By.xpath("//div[@id='product-total']/p/span");
 	private By filterBlock = By.id("filterBlock");
-	private WebElement orderBy = driver.findElement(By.xpath("//div[@id='controls-top']/div[4]/div/label/select"));
+	private By orderBy = By.xpath("//div[@id='controls-top']/div[4]/div/label/select");
 	
 	public AppliancesPage(WebDriver driver) {
 		super(driver);
@@ -33,7 +32,7 @@ public class AppliancesPage extends BasePage {
 	}
 	
 	public String getOrderBySelection() {
-		Select sort = new Select(orderBy);
+		Select sort = new Select(driver.findElement(orderBy));
 		return sort.getFirstSelectedOption().getText();
 	}
 }
