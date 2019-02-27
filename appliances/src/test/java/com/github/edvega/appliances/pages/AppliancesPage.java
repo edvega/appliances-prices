3957cb1d9f96380c43f26d27e1e5ed04896e7161
Edwin Vega <edwega@gmail.com>
2019-02-26 23:25:00 -0600

    Started getFirstFivePrices test in AppliancesPageTest

--- appliances/src/test/java/com/github/edvega/appliances/pages/AppliancesPage.java
+++ appliances/src/test/java/com/github/edvega/appliances/pages/AppliancesPage.java
@@ -1,7 +1,10 @@
 package com.github.edvega.appliances.pages;
 
+import java.util.ArrayList;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import com.github.edvega.appliances.utilities.BasePage;
@@ -12,6 +15,7 @@ public class AppliancesPage extends BasePage {
 	private By productsCounter = By.xpath("//div[@id='product-total']/p/span");
 	private By filterBlock = By.id("filterBlock");
 	private By orderBy = By.xpath("//div[@id='controls-top']/div[4]/div/label/select");
+	private By productList = By.cssSelector(".price-state.price-special");
 	
 	public AppliancesPage(WebDriver driver) {
 		super(driver);
@@ -21,9 +25,8 @@ public class AppliancesPage extends BasePage {
 		return readText(filterHeader);
 	}
 	
-	public AppliancesPage getProductsCounter() {
-		readText(productsCounter);
-		return this;
+	public String getProductsCounter() {
+		return readText(productsCounter);
 	}
 	
 	public AppliancesPage isFiltersPresent() {
@@ -31,8 +34,19 @@ public class AppliancesPage extends BasePage {
 		return this;
 	}
 	
+	public AppliancesPage setOrderBySelection(String visibleText) {
+		Select sort = new Select(driver.findElement(orderBy));
+		sort.selectByVisibleText(visibleText);
+		return this;
+	}
+	
 	public String getOrderBySelection() {
 		Select sort = new Select(driver.findElement(orderBy));
 		return sort.getFirstSelectedOption().getText();
+	}
+	
+	public ArrayList<WebElement> getProductsList() {
+		waitVisibility(productList);
+		return new ArrayList<WebElement>(driver.findElements(productList));
 	}
 }
--- appliances/src/test/java/com/github/edvega/appliances/tests/AppliancesPageTest.java
+++ appliances/src/test/java/com/github/edvega/appliances/tests/AppliancesPageTest.java
@@ -1,5 +1,11 @@
 package com.github.edvega.appliances.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import com.github.edvega.appliances.pages.HomePage;
@@ -11,7 +17,31 @@ public class AppliancesPageTest extends BaseTest {
 	public void verifyFilterHeader() {
 		HomePage home = new HomePage(driver);
 		String title = home.goToHomePage().goToAppliances().getFilterHeader();
-		System.out.println(title);
+		assertEquals(title, "LÍNEA BLANCA");
 	}
 	
+	@Test
+	public void productsCounter() {
+		HomePage home = new HomePage(driver);
+		System.out.println(home.goToHomePage().goToAppliances().
+				getProductsCounter());
+	}
+	
+	@Test
+	public void setOrderOfResults() {
+		HomePage home = new HomePage(driver);
+		home.goToHomePage().goToAppliances().setOrderBySelection("Más Vendido");
+	}
+	
+	@Test(groups = {"results"})
+	public void getFirstFivePrices() {
+		HomePage home = new HomePage(driver);
+		ArrayList<WebElement> list = home.goToHomePage().goToAppliances()
+				.getProductsList();
+		
+		for (WebElement e : list) {
+			System.out.println(e.findElement(By
+					.xpath("//span[@class='price-amount']")).getText());
+		}
+	}
 }
